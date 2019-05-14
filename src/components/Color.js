df11efccb147eb92a92fa09bacd96dd87dd9ab52
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-13 17:22:33 -0700

    colors are rendering

---
+++ src/components/Color.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function Color({ name, hex, red, green, blue }) {
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>{name ? name : hex}</dd>
+
+      <dt>Hex</dt>
+      <dd>{hex}</dd>
+
+      <dt>Red</dt>
+      <dd>{red}</dd>
+
+      <dt>Green</dt>
+      <dd>{green}</dd>
+
+      <dt>Blue</dt>
+      <dd>{blue}</dd>
+    </dl>
+  );
+}
+
+Color.propTypes = {
+  name: propTypes.string.isRequired,
+  hex: propTypes.string.isRequired,
+  red: propTypes.number.isRequired,
+  green: propTypes.number.isRequired,
+  blue: propTypes.number.isRequired,
+};
+
+export default Color;
